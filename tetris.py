bb83809407d4ab19e74a37af754815b6be2d6e94
red-ruby-quay <riviza12@gmail.com>
2022-10-17 22:15:06 +0700

    first commit

---
+++ tetris.py
@@ -0,0 +1,14 @@
+from doctest import DONT_ACCEPT_TRUE_FOR_1
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+from datetime import datetime
+import time
+
+def load_data():
+    
+df_1 = pd.read_excel()
+st.header('PERCUMA LAPOR POLISI?')
+
